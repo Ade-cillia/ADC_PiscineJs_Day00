181b4ed95e47fe6fdc5da4d3f7da51ed8c0e0e7c
Aurélien <aureliendu3809@outlook.fr>
2021-11-29 17:46:50 +0100

    exo04

--- ex04/program.js
+++ ex04/program.js
@@ -4,53 +4,67 @@ function levDist(str1, str2){
         add:0,
         remove:0
     };
+    let maxLen;
+    let str1Change=str1
+    let str2Change = str2
+    let bigStr;
+    if (str1.length>str2.length) {
+        maxLen=str1.length
+        bigStr=str1
+    } else {
+        maxLen=str2.length
+        bigStr=str2
+    }
+    /*
     if (str1.length>str2.length) {
         strChange=str1
-        littleString = str2
+        str2Change = str2
     } else {
         strChange=str2
-        littleString = str1
+        str2Change = str1
     }
-    for (let i = 0; i < strChange.length; i++) {
-        console.log("start-----------")
-        console.log("String change="+strChange)
-        console.log("String objectif="+littleString)
-        //console.log("char="+strChange[i])
-        console.log("index="+i)
-        if ((strChange[i+1] == littleString[i]) && littleString[i]){
-            console.log("Remove");
+    */
+    for (let i = 0; i < maxLen; i++) {
+        //console.log("start-----------")
+        //console.log("String change="+str1Change)
+        //console.log("String objectif="+str2Change)
+        //console.log("char="+str1Change[i])
+        //console.log("index="+i)
+        if ((str1Change[i+1] == str2Change[i]) && str2Change[i]){
+            //console.log("Remove");
             result["remove"] += 1;
-            strChange = strChange.substring(0,i)+strChange.substring(i+1)
+            str1Change = str1Change.substring(0,i)+str1Change.substring(i+1)
             i--; // le remove enlève logiquement un de l'index
             
-        }else if(littleString[i+1] == strChange[i]  && littleString[i]){
-            console.log("Add");
+        }else if(str2Change[i+1] == str1Change[i]  && str2Change[i]){
+            //console.log("Add");
             result["add"] += 1;
-            strChange = strChange.substring(0,i)+littleString[i]+strChange.substring(i)
-        }else if (littleString[i] != strChange[i] && littleString[i]) {
-            console.log("Replace");
+            str1Change = str1Change.substring(0,i)+str2Change[i]+str1Change.substring(i)
+        }else if (str2Change[i] != str1Change[i] && str2Change[i]) {
+            //console.log("Replace");
             result["replace"] += 1;
-            strChange = strChange.substring(0,i)+littleString[i]+strChange.substring(i+1)
+            str1Change = str1Change.substring(0,i)+str2Change[i]+str1Change.substring(i+1)
 
-        }else if (!littleString[i] ) {  // Si la petite string na plus de caractère a partire de i
-            console.log("RemoveEnd"); //litle string finish
+        }else if (str1Change != str2Change && !str2Change[i]) {  // Si la petite string na plus de caractère a partire de i
+            //console.log("RemoveEnd"); //litle string finish
             result["remove"] += 1;
-            strChange =strChange.substring(0,i)+strChange.substring(i+1)
+            str1Change =str1Change.substring(0,i)+str1Change.substring(i+1)
             i--;
             
         }
         //console.log("end-----------")
     }
-    console.log(strChange)
-    console.log(littleString)
+    //console.log(str1Change)
+    //console.log(str2Change)
     //return result
     result["allChange"] = result["replace"]+result["add"]+result["remove"];
-    return result;
+    console.log(result)
+    return result["allChange"];
 }
 
 
-str1 = "moi je mange des pates" 
-str2 = 'lui il mange du fromage'
+str1 = "lui il mange du fromage";
+str2 = 'moi je mange des pates';
 console.log(levDist(str1,str2))
 
 exports.levDist =  levDist;
