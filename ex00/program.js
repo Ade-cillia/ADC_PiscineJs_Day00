18960840e7c2f323b6782224cbb4ddfc98ea0107
Aurélien <aureliendu3809@outlook.fr>
2021-11-29 22:45:23 +0100

    ex00

--- ex00/program.js
+++ ex00/program.js
@@ -10,7 +10,6 @@ function twoNumberSum(array, targetSum){
 }
 array = [ 3, 5, -4, 8, 11, 1, -1, 6];
 targetSum = 10;
-
 console.log(twoNumberSum(array,targetSum));
 
 exports.twoNumberSum =  twoNumberSum;
